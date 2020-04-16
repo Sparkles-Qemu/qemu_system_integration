2d08c1fd8718f81b38fd9d9acec78674b15905bc
galdamez.w@husky.neu.edu <galdamez.w@husky.neu.edu>
2020-04-15 21:54:48 -0400

    Own folder for DMA component

---
+++ src/DMA/DMA.cpp
@@ -0,0 +1,156 @@
+#ifndef DMA_CPP // Note include guards, this is a quick and dirty way to include components
+#define DMA_CPP 
+
+#include "systemc.h"
+#include "map"
+#include "vector"
+#include <string>
+#include <iostream>
+
+//-----------------------------------------------------
+// Design Name : DMA
+// File Name   : DMA.cpp
+// Function    : Direct Memory Access
+// Coder       : Jacob Londa and Owen Beringer
+//-----------------------------------------------------
+
+enum class DmaDirection
+{
+  MM2S,
+  S2MM
+};
+
+enum class DmaState
+{
+  SUSPENDED,             // do nothing indefinitely
+  TRANSFER,              // transfer data
+  WAIT                   // do nothing for certain number of cycles
+};
+
+struct Descriptor
+{
+  sc_uint<32> next;      // index of next descriptor
+  sc_uint<32> start;     // start index in ram array
+  DmaState state;        // state of dma
+  sc_uint<32> x_count;   // number of floats to transfer/wait
+  sc_uint<32> x_modify;  // number of floats between each transfer/wait
+};
+
+// DMA module definition for MM2S and S2MM
+struct DMA : public sc_module
+{
+  // Control Signals
+  sc_in<bool> clk, reset, enable;
+
+  // Memory and Stream
+  float* ram;
+  sc_inout<float> stream;
+
+  // Internal Data
+  std::vector<Descriptor> descriptors;
+  sc_uint<32> execute_index;
+  DmaDirection direction;
+  sc_uint<32> current_ram_index;
+  sc_uint<32> x_count_remaining;
+
+  // Prints descriptor list, useful for debugging
+  void print_descriptors()
+  {
+    for (int i = 0; i < descriptors.size(); i++)
+    {
+      std::cout << "Descriptor " << i << std::endl;
+      std::cout << "next: " << descriptors[i].next << std::endl;
+      std::cout << "start: " << descriptors[i].start << std::endl;
+      std::cout << "state: " << (int)descriptors[i].state << std::endl;
+      std::cout << "x_count: " << descriptors[i].x_count << std::endl;
+      std::cout << "x_modify: " << descriptors[i].x_modify << std::endl << std::endl;
+    }
+  }
+
+  // Called on rising edge of clk or high level reset
+  void update() 
+  {
+   if (reset.read()) 
+    {
+      // assume at least one descriptor is in dma at all times
+      execute_index = 0;
+      current_ram_index = descriptors[execute_index].start;
+      x_count_remaining = descriptors[execute_index].x_count;
+      descriptors[execute_index].state == DmaState::SUSPENDED;  // slightly cheating here, but does what we want
+      std::cout << "@ " << sc_time_stamp() << " " << this->name() << ": Module has been reset" << std::endl;
+    } 
+    else if (enable.read() && (descriptors[execute_index].state != DmaState::SUSPENDED))
+    {
+      if (descriptors[execute_index].state == DmaState::TRANSFER)
+      {
+        if (direction == DmaDirection::MM2S)  // Memory to Stream
+        {
+          float value = *(ram + current_ram_index);
+          std::cout << "@ " << sc_time_stamp() << " " << this->name() << " desc " << execute_index << ": Transfering [" << value << "] from RAM to stream" << std::endl;
+          stream.write(value);
+        }
+        else  // Stream to Memory
+        {
+          std::cout << "@ " << sc_time_stamp() << " " << this->name() << " desc " << execute_index << ": Transfering [" << stream.read() << "] from stream to RAM" << std::endl;
+          *(ram + current_ram_index) = stream.read();
+        }
+
+        // update ram index
+        current_ram_index += descriptors[execute_index].x_modify;
+      }
+      else  // just for debugging, can be removed
+        std::cout << "@ " << sc_time_stamp() << " " << this->name() << " desc " << execute_index << ": Waiting..." << std::endl;
+      
+      x_count_remaining--;
+
+      if (x_count_remaining == 0)  // descriptor is finished, load next descriptor
+      {
+        execute_index = descriptors[execute_index].next;
+        current_ram_index = descriptors[execute_index].start;
+        x_count_remaining = descriptors[execute_index].x_count;
+      }
+    }
+  }
+
+  // Constructor
+  DMA(sc_module_name name, DmaDirection _direction, const sc_signal<bool>& _clk, const sc_signal<bool>& _reset,const sc_signal<bool>& _enable, float* _ram, sc_signal<float,SC_MANY_WRITERS>& _stream)
+  {
+      SC_METHOD(update);
+        sensitive << reset;
+        sensitive << clk.pos();
+      
+      // connect signals
+      this->direction = _direction;
+      this->clk(_clk);
+      this->reset(_reset);
+      this->enable(_enable);
+      this->ram = _ram;
+      this->stream(_stream);
+
+      std::cout << "Module: " << name << " has been instantiated " << std::endl;
+  }
+
+  SC_HAS_PROCESS(DMA);
+};
+
+// DMA module definition for MM2MM
+struct DMA_MM2MM : public sc_module
+{
+	// stream interconnect
+	sc_signal<float,SC_MANY_WRITERS> stream;
+
+	// DMA devices
+	DMA mm2s, s2mm;
+
+	// Constructor
+	DMA_MM2MM(sc_module_name name, const sc_signal<bool>& _clk, const sc_signal<bool>& _reset, const sc_signal<bool>& _enable, float* _ram_source, float* _ram_destination) :
+    mm2s("internal_mm2s", DmaDirection::MM2S, _clk, _reset, _enable, _ram_source, stream),
+    s2mm("internal_s2mm", DmaDirection::S2MM, _clk, _reset, _enable, _ram_destination, stream)
+	{
+		std::cout << "Module: " << name << " has been instantiated" << std::endl;
+	}
+
+	SC_HAS_PROCESS(DMA_MM2MM);
+};
+
+#endif  // DMA_CPP
---
+++ src/DMA/SIM.cpp
@@ -0,0 +1,58 @@
+#include "systemc.h"
+#include "DMA.cpp"
+#include "DMAtb.h"
+
+using std::cout;
+using std::endl;
+
+#define MAX_RESET_CYCLES 10
+#define MAX_SIM_CYCLES 20
+
+/**
+ * @brief Simulation entry point note sc_main not main.... 
+ * a systemc idiosyncrasy related to the linking behavior. 
+ * 
+ * @param argc number of arguments passed (just like regular C )
+ * @param argv arguments passed (just like regular C )
+ * @return int 
+ */
+int sc_main(int argc, char *argv[]) 
+{
+	sc_signal<bool> clk("clk"); 
+	sc_signal<bool> reset("reset"); 
+	sc_signal<bool> enable("enable"); 
+	sc_signal<float, SC_MANY_WRITERS> stream("stream");	// AC_MANY_WRITERS allows stream to have numerous drivers, (dma and dma_test)
+	
+	// RAM representing external Memory
+	float ram[100];
+
+	// Fill ram with 1-100
+	int i;
+	for (i = 0; i < 100; i++)
+		ram[i] = i;
+
+	// DMA for MM2S S2MM instantiation
+	DMA dma_temp("dma_temp", DmaDirection::MM2S, clk, reset, enable, ram, stream);
+	Descriptor d1 = {1, 0, DmaState::SUSPENDED, 3, 1}; // Test Suspended State
+	Descriptor d2 = {2, 50, DmaState::WAIT, 5, 2};     // Test Waiting State
+	Descriptor d3 = {3, 10, DmaState::TRANSFER, 2, 10};// Test Standard access
+	Descriptor d4 = {0, 89, DmaState::TRANSFER, 11, 1};// This should overflow ram
+	dma_temp.descriptors.push_back(d1);
+	dma_temp.descriptors.push_back(d2);
+	dma_temp.descriptors.push_back(d3);
+	dma_temp.descriptors.push_back(d4);
+	dma_temp.print_descriptors();
+	
+	// DMA MM2S and S2MM Testbench
+	dma_test test("dma_test", dma_temp, ram);
+	test.clk(clk);
+	test.enable(enable);
+	test.reset(reset);
+	test.stream(stream);
+
+	sc_start();//1, SC_NS); // Run Test
+
+	return 0;
+}
+
+
