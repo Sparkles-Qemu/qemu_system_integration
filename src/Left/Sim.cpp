f25c98fa77c47a1d478f16d4dc258e018ee3b96c
Jacob Londa <jelonda@gmail.com>
2020-04-20 18:22:25 -0400

    adding descriptors

--- src/Left/Sim.cpp
+++ src/Left/Sim.cpp
@@ -1,6 +1,9 @@
 #include <systemc.h>
+#include "DMA.cpp"
 #include "LEFT.cpp"
 
+#define SMALL_RAM_SIZE 10
+#define BIG_RAM_SIZE 3 * SMALL_RAM_SIZE
 
 int sc_main(int argc, char *argv[])
 {
@@ -14,15 +17,43 @@ int sc_main(int argc, char *argv[])
 	enable.write(0);
 
 	// RAM representing external Memory
-	float ram[100], ram1[100] = {0}, ram2[100] = {0}, ram3[100] = {0};
+	float ram[BIG_RAM_SIZE], ram1[SMALL_RAM_SIZE] = {0}, ram2[SMALL_RAM_SIZE] = {0}, ram3[SMALL_RAM_SIZE] = {0};
 
-	// Fill ram with 1-100
+	// Fill source ram
 	int i;
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < BIG_RAM_SIZE; i++)
 		ram[i] = i;
 
 	// instantiate left side of processor
 	LEFT left("left_side", clk, reset, enable, ram, ram1, ram2, ram3);
+
+	// descriptors for source ram
+	Descriptor desc_mm2s = {0, 0, DmaState::TRANSFER, BIG_RAM_SIZE, 1};
+	left.dma_mm2s.descriptors.push_back(desc_mm2s);
+
+	// descriptors for destination ram 1
+	Descriptor desc_s2mm1_wait_before = {1, 0, DmaState::WAIT, 1, 1};
+	Descriptor desc_s2mm1_transfer = {2, 0, DmaState::TRANSFER, SMALL_RAM_SIZE, 1};	
+	Descriptor desc_s2mm1_sus_after = {0, 0, DmaState::SUSPENDED, 0, 1};
+	left.dma_s2mm1.descriptors.push_back(desc_s2mm1_wait_before);
+	left.dma_s2mm1.descriptors.push_back(desc_s2mm1_transfer);
+	left.dma_s2mm1.descriptors.push_back(desc_s2mm1_sus_after);
+
+	// descriptors for destination ram 2
+	Descriptor desc_s2mm2_wait_before = {1, 0, DmaState::WAIT, 1 + SMALL_RAM_SIZE, 1};
+	Descriptor desc_s2mm2_transfer = {2, 0, DmaState::TRANSFER, SMALL_RAM_SIZE, 1};	
+	Descriptor desc_s2mm2_sus_after = {0, 0, DmaState::SUSPENDED, 0, 1};
+	left.dma_s2mm2.descriptors.push_back(desc_s2mm2_wait_before);
+	left.dma_s2mm2.descriptors.push_back(desc_s2mm2_transfer);
+	left.dma_s2mm2.descriptors.push_back(desc_s2mm2_sus_after);
+
+	// descriptors for destination ram 3
+	Descriptor desc_s2mm3_wait_before = {1, 0, DmaState::WAIT, 1 + 2 * SMALL_RAM_SIZE, 1};
+	Descriptor desc_s2mm3_transfer = {2, 0, DmaState::TRANSFER, SMALL_RAM_SIZE, 1};	
+	Descriptor desc_s2mm3_sus_after = {0, 0, DmaState::SUSPENDED, 0, 1};
+	left.dma_s2mm3.descriptors.push_back(desc_s2mm3_wait_before);
+	left.dma_s2mm3.descriptors.push_back(desc_s2mm3_transfer);
+	left.dma_s2mm3.descriptors.push_back(desc_s2mm3_sus_after);
 
 	return 0;
 
