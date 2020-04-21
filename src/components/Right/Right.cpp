eb9387886645093295faa01ace2be2a327671124
asultan123 <asultan@aucegypt.edu>
2020-04-21 17:20:39 -0400

    update RIGHT component with 9 compute branch components

--- src/components/Right/Right.cpp
+++ src/components/Right/Right.cpp
@@ -83,13 +83,19 @@ SC_MODULE(RIGHT)
 	// Stream from MM2S to S2MMs
   	sc_in<bool> clk, reset, enable;
 	sc_out<float> streamOut;
-	const unsigned int branchCount = 3;
+	const unsigned int branchCount = 9;
 	sc_vector<sc_signal<float> > interComputeBranchPsum;
-	COMPUTE_BRANCH branch0;
-	COMPUTE_BRANCH branch1;
-	COMPUTE_BRANCH branch2;
+	COMPUTE_BRANCH branch00;
+	COMPUTE_BRANCH branch01;
+	COMPUTE_BRANCH branch02;
+	COMPUTE_BRANCH branch10;
+	COMPUTE_BRANCH branch11;
+	COMPUTE_BRANCH branch12;
+	COMPUTE_BRANCH branch20;
+	COMPUTE_BRANCH branch21;
+	COMPUTE_BRANCH branch22;    
 
-    void update() {
+	void update() {
         if(reset.read() == 1) {
             streamOut.write(0);
         } else if (enable.read() == 1) {
@@ -101,10 +107,15 @@ SC_MODULE(RIGHT)
 	// Constructor with init list of components
 	RIGHT(sc_module_name name, const sc_signal<bool>& _clk, const sc_signal<bool>& _reset, const sc_signal<bool>& _enable, float* _ram_source0, float* _ram_source1, float* _ram_source2, sc_signal<float>& _streamOut) : \
 		interComputeBranchPsum("interComputeBranchPsum", branchCount+1),
-		branch0("branch0", _clk,  _reset,  _enable, interComputeBranchPsum[0], interComputeBranchPsum[1],  _ram_source0, 3),
-		branch1("branch1", _clk,  _reset,  _enable, interComputeBranchPsum[1], interComputeBranchPsum[2],  _ram_source1, 3),
-		branch2("branch2", _clk,  _reset,  _enable, interComputeBranchPsum[2], interComputeBranchPsum[3],  _ram_source2, 3)
-
+		branch00("branch00", _clk,  _reset,  _enable, interComputeBranchPsum[0], interComputeBranchPsum[1],  _ram_source0, 3),
+		branch01("branch01", _clk,  _reset,  _enable, interComputeBranchPsum[1], interComputeBranchPsum[2],  _ram_source0, 3),
+		branch02("branch02", _clk,  _reset,  _enable, interComputeBranchPsum[2], interComputeBranchPsum[3],  _ram_source0, 3),
+		branch10("branch10", _clk,  _reset,  _enable, interComputeBranchPsum[3], interComputeBranchPsum[4],  _ram_source1, 3),
+		branch11("branch11", _clk,  _reset,  _enable, interComputeBranchPsum[4], interComputeBranchPsum[5],  _ram_source1, 3),
+		branch12("branch12", _clk,  _reset,  _enable, interComputeBranchPsum[5], interComputeBranchPsum[6],  _ram_source1, 3),
+		branch20("branch20", _clk,  _reset,  _enable, interComputeBranchPsum[6], interComputeBranchPsum[7],  _ram_source2, 3),
+		branch21("branch21", _clk,  _reset,  _enable, interComputeBranchPsum[7], interComputeBranchPsum[8],  _ram_source2, 3),
+		branch22("branch22", _clk,  _reset,  _enable, interComputeBranchPsum[8], interComputeBranchPsum[9],  _ram_source2, 3)
 	{
 		clk(_clk);
 		reset(_reset);
