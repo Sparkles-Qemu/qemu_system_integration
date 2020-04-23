b44efdba3b7f078a0b835c4d3e8ee36e1f5e0bbd
asultan123 <asultan@aucegypt.edu>
2020-04-23 02:40:17 -0400

    created formal processor component and moved code from tb_processor to Processor.cpp

--- src/components/Processor/Processor.cpp
+++ src/components/Processor/Processor.cpp
@@ -1,192 +1,36 @@
-#ifndef RIGHT_CPP
-#define RIGHT_CPP 
+#ifndef PROCESSOR_CPP //be carefule here, if there are duplicates used below they won't be used
+#define PROCESSOR_CPP 
 
 #include <systemc.h>
 #include <string>
-#include <DMA.cpp>
-#include <maa.cpp>
-#include <iostream>
+#include "Right.cpp"
+#include "LEFT.cpp"
 
-struct PE_GROUP : public sc_module 
+#define IMAGE_WIDTH 10
+#define IMAGE_HEIGHT 10
+#define IMAGE_SIZE IMAGE_WIDTH*IMAGE_HEIGHT
+#define SMALL_RAM_SIZE IMAGE_SIZE 
+#define BIG_RAM_SIZE 3 * IMAGE_SIZE
+#define MEMORY_PADDING 100
+
+struct Processor : public sc_module 
 {
-	// Stream from MM2S to S2MMs
-	sc_in<float> psumIn;
-  	sc_in<bool> clk, reset, enable;
-	sc_out<float> psumOut;
+	sc_signal<float> streamOut;
 
-	sc_signal<float,SC_MANY_WRITERS> pixelInBus;
-	sc_vector<sc_signal<float> > interPsumSignals;
-	sc_vector<mult_accumulate > peArray;
-	unsigned int peArraySize;
+	float ramSource[BIG_RAM_SIZE+MEMORY_PADDING] = {0};
+	float ram0[SMALL_RAM_SIZE+MEMORY_PADDING] = {0};
+	float ram1[SMALL_RAM_SIZE+MEMORY_PADDING] = {0};
+	float ram2[SMALL_RAM_SIZE+MEMORY_PADDING] = {0};
 
-	// Memory To Stream
-	DMA dma_mm2s;
+	LEFT left;
+	RIGHT right;
 
-    void update() {
-        if(reset.read() == 1) {
-            psumOut.write(0);
-        } else if (enable.read() == 1) {
-            interPsumSignals[0] = psumIn.read();
-            psumOut.write(interPsumSignals[peArraySize]);
-        }
-    }
-
-    void loadWeights(std::vector<float> weights)
-    {
-    	for(unsigned int i = 0; i<peArraySize && i<weights.size(); i++)
-    	{
-    		peArray[i].weight = weights[i];
-    	}
-    }
-
-	SC_HAS_PROCESS(PE_GROUP);
-
-	// Constructor with init list of components
-	PE_GROUP(sc_core::sc_module_name name, const sc_signal<bool>& _clk, const sc_signal<bool>& _reset, const sc_signal<bool>& _enable, const sc_signal<float>& _psumIn, sc_signal<float>& _psumOut, float* _ram_source, unsigned int arraySize) : 
-	sc_module(name),
-	interPsumSignals("interPsumSignals", arraySize+1),
-	peArray("peArray", arraySize),
-	dma_mm2s("internal_mm2pixelInBus", DmaDirection::MM2S, _clk, _reset, _enable, _ram_source, pixelInBus)
-	{
-		// std::cout << "PE_GROUP Module: " << name << "attempting to be instantiated" << std::endl;
-
-    	SC_METHOD(update);
-        // sensitive << clk.pos();
-        // sensitive << reset;
-
-		peArraySize = arraySize;
-
-        sensitive << interPsumSignals[peArraySize];
-        sensitive << psumIn;
-        sensitive << reset;
-
-		psumIn(_psumIn);
-
-		clk(_clk);
-		reset(_reset);
-		enable(_enable);
-
-		for(unsigned int i = 0; i<arraySize; i++)
-		{
-		    peArray[i].pixelIn(pixelInBus);
-		    peArray[i].enable(_enable);
-		    peArray[i].reset(_reset);
-		    peArray[i].clk(_clk);
-		    peArray[i].partialSum(interPsumSignals[i]);
-		    peArray[i].output(interPsumSignals[i+1]);
-		}
-		psumOut(_psumOut);
-
-		std::cout << "PE_GROUP Module: " << name << " has been instantiated" << std::endl;
-		
-	}
-
-
-};
-
-struct PE_CLOUD : public sc_module 
-{
-	// Stream from MM2S to S2MMs
-	sc_in<float> psumIn;
-  	sc_in<bool> clk, reset, enable;
-	sc_out<float> psumOut;
-
-	const unsigned int groupCount = 3;
-
-	sc_vector<sc_signal<float> > interGroupSignals;
-
-	PE_GROUP group0;
-	PE_GROUP group1;
-	PE_GROUP group2;
-
-    void update() {
-        if(reset.read() == 1) {
-            psumOut.write(0);
-        } else if (enable.read() == 1) {
-            interGroupSignals[0] = psumIn.read();
-            psumOut.write(interGroupSignals[groupCount]);
-        }
-    }
-
-	SC_HAS_PROCESS(PE_CLOUD);
-
-	// Constructor with init list of components
-	PE_CLOUD(sc_module_name name, const sc_signal<bool>& _clk, const sc_signal<bool>& _reset, const sc_signal<bool>& _enable, const sc_signal<float>& _psumIn, sc_signal<float>& _psumOut, float* _ram_source) : 
-	sc_module(name), 
-	interGroupSignals("interGroupSignals", groupCount+1), 
-	group0("group0", _clk, _reset, _enable, interGroupSignals[0], interGroupSignals[1], _ram_source, 3), 
-	group1("group1", _clk, _reset, _enable, interGroupSignals[1], interGroupSignals[2], _ram_source, 3), 
-	group2("group2", _clk, _reset, _enable, interGroupSignals[2], interGroupSignals[3], _ram_source, 3)		
-	{
-	    // std::cout << "PE_CLOUD Module: " << name << "attempting to be instantiated" << std::endl;
-
-    	SC_METHOD(update);
-        // sensitive << clk.pos();
-        // sensitive << reset;
-        sensitive << interGroupSignals[groupCount];
-        sensitive << psumIn;
-        sensitive << reset;
-
-		psumIn(_psumIn);
-
-		clk(_clk);
-		reset(_reset);
-		enable(_enable);
-
-		psumOut(_psumOut);
-
-		std::cout << "PE_CLOUD Module: " << name << " has been instantiated" << std::endl;
-		
-	}
-
-
-};
-
-struct RIGHT : public sc_module 
-{
-	// Stream from MM2S to S2MMs
-  	sc_in<bool> clk, reset, enable;
-	sc_out<float> streamOut;
-	const unsigned int branchCount = 3;
-	sc_vector<sc_signal<float> > interComputeBranchPsum;
-
-	PE_CLOUD branch0;
-	PE_CLOUD branch1;
-	PE_CLOUD branch2;
-
-	void update() {
-        if(reset.read() == 1) {
-            streamOut.write(0);
-            interComputeBranchPsum[0] = 0;
-        } else if (enable.read() == 1) {
-            interComputeBranchPsum[0] = 0;
-            streamOut.write(interComputeBranchPsum[branchCount]);
-        }
-    }
-
-	// Constructor with init list of components
-	RIGHT(sc_core::sc_module_name name, const sc_signal<bool>& _clk, const sc_signal<bool>& _reset, const sc_signal<bool>& _enable, float* _ram_source0, float* _ram_source1, float* _ram_source2, sc_signal<float>& _streamOut) : 
+	Processor(sc_core::sc_module_name name, const sc_signal<bool>& _clk, const sc_signal<bool>& _reset, const sc_signal<bool>& _enable) : 
 		sc_module(name),
-		interComputeBranchPsum("interComputeBranchPsum", branchCount+1),
-		branch0("branch0",  _clk, _reset, _enable, interComputeBranchPsum[0], interComputeBranchPsum[1], _ram_source0),
-		branch1("branch1",  _clk, _reset, _enable, interComputeBranchPsum[1], interComputeBranchPsum[2], _ram_source1),
-		branch2("branch2",  _clk, _reset, _enable, interComputeBranchPsum[2], interComputeBranchPsum[3], _ram_source2)
+		left("mem_heirarchy", _clk, _reset, _enable, ramSource, ram0, ram1, ram2),
+		right("compute", _clk, _reset, _enable, ram0, ram1, ram2, streamOut)
 	{
-	    // std::cout << "RIGHT Module: " << name << "attempting to be instantiated" << std::endl;
-		clk(_clk);
-		reset(_reset);
-		enable(_enable);
-
-		streamOut(_streamOut);
-
-    	SC_METHOD(update);
-        // sensitive << clk.pos();
-        // sensitive << reset;
-
-        sensitive << interComputeBranchPsum[branchCount];
-        sensitive << reset;
-
-		std::cout << "RIGHT Module: " << name << " has been instantiated" << std::endl;
+		std::cout << "Processor Module: " << name << " has been instantiated" << std::endl;
 	}
 	
 	SC_HAS_PROCESS(RIGHT);
--- src/components/Processor/tb_processor.cpp
+++ src/components/Processor/tb_processor.cpp
@@ -1,47 +1,25 @@
 #include <systemc.h>
-#include "Right.cpp"
-#include "LEFT.cpp"
-
-#define IMAGE_WIDTH 10
-#define IMAGE_HEIGHT 10
-#define IMAGE_SIZE IMAGE_WIDTH*IMAGE_HEIGHT
-#define SMALL_RAM_SIZE IMAGE_SIZE 
-#define BIG_RAM_SIZE 3 * IMAGE_SIZE
-#define MEMORY_PADDING 100
-
-void print_ram(float* ram, int size)
-
-{
-	for (int i = 0; i < size; i++)
-		std::cout << *(ram + i) << " ";
-	std::cout << "\n\n";
-}
+#include "Processor.cpp"
 
 int sc_main(int argc, char *argv[])
 {
+
+  std::cout << "Starting Testbench " << std::endl;
+
 	sc_signal<bool> clk("clk"); 
 	sc_signal<bool> reset("reset"); 
 	sc_signal<bool> enable("enable"); 
-	sc_signal<float> stream_in("stream_in");	
-  sc_signal<float> stream_out("stream_out");
-  sc_signal<float> ground("gnd");
-	
-  float ramSource[BIG_RAM_SIZE+MEMORY_PADDING] = {0};
-  float ram0[SMALL_RAM_SIZE+MEMORY_PADDING] = {0};
-  float ram1[SMALL_RAM_SIZE+MEMORY_PADDING] = {0};
-  float ram2[SMALL_RAM_SIZE+MEMORY_PADDING] = {0};
 
   float expected_output[64] = {15678, 15813, 15948, 16083, 16218, 16353, 16488, 16623, 17028, 17163, 17298, 17433, 17568, 17703, 17838, 17973, 18378, 18513, 18648, 18783, 18918, 19053, 19188, 19323, 19728, 19863, 19998, 20133, 20268, 20403, 20538, 20673, 21078, 21213, 21348, 21483, 21618, 21753, 21888, 22023, 22428, 22563, 22698, 22833, 22968, 23103, 23238, 23373, 23778, 23913, 24048, 24183, 24318, 24453, 24588, 24723, 25128, 25263, 25398, 25533, 25668, 25803, 25938, 26073};
+
+  Processor processor("processor", clk, reset, enable); 
   
 	for (int i = 0; i < BIG_RAM_SIZE ; i++)
   {
-    ramSource[i] = i+1;
+    processor.ramSource[i] = i+1;
   }
 
-  RIGHT cloud("compute", clk, reset, enable, ram0, ram1, ram2, stream_out) ;
-  LEFT left("mem_heirarchy", clk, reset, enable, ramSource, ram0, ram1, ram2);
-
-    // descriptors for source ram
+  // descriptors for source ram
   Descriptor desc_mm2s = {0, 0, DmaState::TRANSFER, BIG_RAM_SIZE, 1};
 
   // descriptors for destination ram 1
@@ -107,7 +85,7 @@ int sc_main(int argc, char *argv[])
   sc_trace(wf, enable, "enable");
   sc_trace(wf, reset, "reset");
   sc_trace(wf, clk, "clk");
-  sc_trace(wf, stream_out, "output");
+  sc_trace(wf, processor.streamOut, "processor.streamOut");
 
   // Start simulation 
   enable = 0;
@@ -120,36 +98,34 @@ int sc_main(int argc, char *argv[])
   sc_start(1, SC_NS);
 
     // load descriptors
-  left.dma_mm2s.loadProgram({desc_mm2s});
-  left.dma_s2mm1.loadProgram({desc_s2mm1_wait_before, desc_s2mm1_transfer, desc_s2mm1_sus_after});
-  left.dma_s2mm2.loadProgram({desc_s2mm2_wait_before, desc_s2mm2_transfer, desc_s2mm2_sus_after});
-  left.dma_s2mm3.loadProgram({desc_s2mm3_wait_before, desc_s2mm3_transfer, desc_s2mm3_sus_after});
+  processor.left.dma_mm2s.loadProgram({desc_mm2s});
+  processor.left.dma_s2mm1.loadProgram({desc_s2mm1_wait_before, desc_s2mm1_transfer, desc_s2mm1_sus_after});
+  processor.left.dma_s2mm2.loadProgram({desc_s2mm2_wait_before, desc_s2mm2_transfer, desc_s2mm2_sus_after});
+  processor.left.dma_s2mm3.loadProgram({desc_s2mm3_wait_before, desc_s2mm3_transfer, desc_s2mm3_sus_after});
   
-  cloud.branch0.group0.dma_mm2s.loadProgram({desc_branch0_group0_timed_wait, desc_branch0_group0_transfer, desc_branch0_group0_suspend});
-  cloud.branch0.group1.dma_mm2s.loadProgram({desc_branch0_group1_timed_wait, desc_branch0_group1_transfer, desc_branch0_group1_suspend});
-  cloud.branch0.group2.dma_mm2s.loadProgram({desc_branch0_group2_timed_wait, desc_branch0_group2_transfer, desc_branch0_group2_suspend});
+  processor.right.branch0.group0.dma_mm2s.loadProgram({desc_branch0_group0_timed_wait, desc_branch0_group0_transfer, desc_branch0_group0_suspend});
+  processor.right.branch0.group1.dma_mm2s.loadProgram({desc_branch0_group1_timed_wait, desc_branch0_group1_transfer, desc_branch0_group1_suspend});
+  processor.right.branch0.group2.dma_mm2s.loadProgram({desc_branch0_group2_timed_wait, desc_branch0_group2_transfer, desc_branch0_group2_suspend});
 
-  cloud.branch1.group0.dma_mm2s.loadProgram({desc_branch1_group0_timed_wait, desc_branch1_group0_transfer, desc_branch1_group0_suspend});
-  cloud.branch1.group1.dma_mm2s.loadProgram({desc_branch1_group1_timed_wait, desc_branch1_group1_transfer, desc_branch1_group1_suspend});
-  cloud.branch1.group2.dma_mm2s.loadProgram({desc_branch1_group2_timed_wait, desc_branch1_group2_transfer, desc_branch1_group2_suspend});
+  processor.right.branch1.group0.dma_mm2s.loadProgram({desc_branch1_group0_timed_wait, desc_branch1_group0_transfer, desc_branch1_group0_suspend});
+  processor.right.branch1.group1.dma_mm2s.loadProgram({desc_branch1_group1_timed_wait, desc_branch1_group1_transfer, desc_branch1_group1_suspend});
+  processor.right.branch1.group2.dma_mm2s.loadProgram({desc_branch1_group2_timed_wait, desc_branch1_group2_transfer, desc_branch1_group2_suspend});
 
-  cloud.branch2.group0.dma_mm2s.loadProgram({desc_branch2_group0_timed_wait, desc_branch2_group0_transfer, desc_branch2_group0_suspend});
-  cloud.branch2.group1.dma_mm2s.loadProgram({desc_branch2_group1_timed_wait, desc_branch2_group1_transfer, desc_branch2_group1_suspend});
-  cloud.branch2.group2.dma_mm2s.loadProgram({desc_branch2_group2_timed_wait, desc_branch2_group2_transfer, desc_branch2_group2_suspend});
+  processor.right.branch2.group0.dma_mm2s.loadProgram({desc_branch2_group0_timed_wait, desc_branch2_group0_transfer, desc_branch2_group0_suspend});
+  processor.right.branch2.group1.dma_mm2s.loadProgram({desc_branch2_group1_timed_wait, desc_branch2_group1_transfer, desc_branch2_group1_suspend});
+  processor.right.branch2.group2.dma_mm2s.loadProgram({desc_branch2_group2_timed_wait, desc_branch2_group2_transfer, desc_branch2_group2_suspend});
 
-  cloud.branch0.group0.loadWeights({1,2,3});
-  cloud.branch0.group1.loadWeights({4,5,6});
-  cloud.branch0.group2.loadWeights({7,8,9});
+  processor.right.branch0.group0.loadWeights({1,2,3});
+  processor.right.branch0.group1.loadWeights({4,5,6});
+  processor.right.branch0.group2.loadWeights({7,8,9});
 
-  cloud.branch1.group0.loadWeights({1,2,3});
-  cloud.branch1.group1.loadWeights({4,5,6});
-  cloud.branch1.group2.loadWeights({7,8,9});
+  processor.right.branch1.group0.loadWeights({1,2,3});
+  processor.right.branch1.group1.loadWeights({4,5,6});
+  processor.right.branch1.group2.loadWeights({7,8,9});
 
-  cloud.branch2.group0.loadWeights({1,2,3});
-  cloud.branch2.group1.loadWeights({4,5,6});
-  cloud.branch2.group2.loadWeights({7,8,9});
-
-  stream_in = 0;
+  processor.right.branch2.group0.loadWeights({1,2,3});
+  processor.right.branch2.group1.loadWeights({4,5,6});
+  processor.right.branch2.group2.loadWeights({7,8,9});
 
   std::cout << "@" << sc_time_stamp() << " Load Pulse " << std::endl;
 
@@ -184,7 +160,7 @@ int sc_main(int argc, char *argv[])
     clk = 0;
     sc_start(0.5, SC_NS);
 
-    if(!startValidation && expected_output[0] == stream_out)
+    if(!startValidation && expected_output[0] == processor.streamOut)
     {
       startValidation = true;
     }
@@ -193,10 +169,10 @@ int sc_main(int argc, char *argv[])
     {
       if(validCounter > 0)
       {
-        std::cout << "@" << sc_time_stamp() << " stream_out: " << stream_out \
+        std::cout << "@" << sc_time_stamp() << " processor.streamOut: " << processor.streamOut \
         << " expected_output: " << expected_output[expected_output_index];
 
-        if(expected_output[expected_output_index] == stream_out)
+        if(expected_output[expected_output_index] == processor.streamOut)
         {
           std::cout << " ....assertion succuess!!" << std::endl;
           expected_output_index++;
@@ -210,13 +186,13 @@ int sc_main(int argc, char *argv[])
       }
       else if(invalidCounter > 0)
       {
-        std::cout << "@" << sc_time_stamp() << " stream_out: " << stream_out \
+        std::cout << "@" << sc_time_stamp() << " processor.streamOut: " << processor.streamOut \
         << " ....ignoring invalid output " << std::endl;
         invalidCounter--;
       }
       else
       {
-        std::cout << "@" << sc_time_stamp() << " stream_out: " << stream_out \
+        std::cout << "@" << sc_time_stamp() << " processor.streamOut: " << processor.streamOut \
         << " ....ignoring invalid output " << std::endl;
         validCounter = 8;
         invalidCounter = 1;
@@ -227,43 +203,43 @@ int sc_main(int argc, char *argv[])
 
   if(expected_output_index == 64)
   {
-    std::cout << "TEST BENCH SUCCESS " << std::endl;
+    std::cout << "TEST BENCH SUCCESS " << std::endl << std::endl;
 
 
-    std::cout << "       a$$$$$$$$$$a" << std::endl;
-    std::cout << "     a$$          $$a" << std::endl;
-    std::cout << "   a$$  $$$$$$$$$$  $$a" << std::endl;
-    std::cout << "  a$$$$$$$$$$$$$$$$$$$a" << std::endl;
-    std::cout << " a$$$$$   $$$$$$   $$$$$a" << std::endl;
-    std::cout << "a$$$$$     $$$$     $$$$$a" << std::endl;
-    std::cout << "a$$$$$$$$$$$$$$$$$$$$$$$$a" << std::endl;
-    std::cout << "a$$$$$$$$$$$$$$$$$$$$$$$$a" << std::endl;
-    std::cout << "a$$$$$$   $$ $$    $$$$$$$a" << std::endl;
-    std::cout << " a$$$$$$  $$ $$   $$$$$$$a" << std::endl;
-    std::cout << "  a$$$$$$        $$$$$$a" << std::endl;
-    std::cout << "   a$$$$$$$$$$$$$$$$$$a" << std::endl;
-    std::cout << "     a$$$$$$$$$$$$$$a" << std::endl;
-    std::cout << "       a$$$$$$$$$$a" << std::endl;
+    std::cout << "       aOOOOOOOOOOa" << std::endl;
+    std::cout << "     aOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << "   aOO    OOOOOO    OOa" << std::endl;
+    std::cout << "  aOOOOOOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << " aOOOOO   OOOOOO   OOOOOa" << std::endl;
+    std::cout << "aOOOOO     OOOO     OOOOOa" << std::endl;
+    std::cout << "aOOOOOOOOOOOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << "aOOOOOOOOOOOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << "aOOOOO   OOOOOOOO   OOOOOa" << std::endl;
+    std::cout << " aOOOOO    OOOO    OOOOOa" << std::endl;
+    std::cout << "  aOOOOO          OOOOOa" << std::endl;
+    std::cout << "   aOOOOOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << "     aOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << "       aOOOOOOOOOOa" << std::endl;
 
   }
   else
   {
-    std::cout << "TEST BENCH FAILURE " << std::endl;
+    std::cout << "TEST BENCH FAILURE " << std::endl  << std::endl;
 
-    std::cout << "       a$$$$$$$$$$a" << std::endl;
-    std::cout << "     a$$$$$$$$$$$$$$a" << std::endl;
-    std::cout << "   a$$$$$$$$$$$$$$$$$$a" << std::endl;
-    std::cout << "  a$$$$$$$$$$$$$$$$$$$$a" << std::endl;
-    std::cout << " a$$$$$   $$$$$$   $$$$$a" << std::endl;
-    std::cout << "a$$$$$     $$$$     $$$$$a" << std::endl;
-    std::cout << "a$$$$$$$ $$$$$$$$$$$$$$$$a" << std::endl;
-    std::cout << "a$$$$$$$ $$$$$$$$$$$$$$$$a" << std::endl;
-    std::cout << "a$$$$$$$$$$$$$$$$$$$$$$$$a" << std::endl;
-    std::cout << " a$$$$$$          $$$$$$a" << std::endl;
-    std::cout << "  a$$$$  $$$$$$$$  $$$$a" << std::endl;
-    std::cout << "   a$$ $$$$$$$$$$$$ $$a" << std::endl;
-    std::cout << "     a$$$$$$$$$$$$$$a" << std::endl;
-    std::cout << "       a$$$$$$$$$$a" << std::endl;
+    std::cout << "       aOOOOOOOOOOa" << std::endl;
+    std::cout << "     aOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << "   aOOOOOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << "  aOOOOOOOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << " aOOOOO   OOOOOO   OOOOOa" << std::endl;
+    std::cout << "aOOOOO     OOOO     OOOOOa" << std::endl;
+    std::cout << "aOOOOOOO OOOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << "aOOOOOOO OOOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << "aOOOOOOOOOOOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << " aOOOOOO          OOOOOOa" << std::endl;
+    std::cout << "  aOOOO  OOOOOOOO  OOOOa" << std::endl;
+    std::cout << "   aOO OOOOOOOOOOOO OOa" << std::endl;
+    std::cout << "     aOOOOOOOOOOOOOOa" << std::endl;
+    std::cout << "       aOOOOOOOOOOa" << std::endl;
   }
 
   return 0;
