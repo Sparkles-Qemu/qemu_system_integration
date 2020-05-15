0dc78301016088a7dc5790d86c698f4560704601
asultan123 <asultan@aucegypt.edu>
2020-05-15 18:06:00 -0400

    began testbench ... really dragging this out

--- src/components/DMA/tb_2DDMA.cpp
+++ src/components/DMA/tb_2DDMA.cpp
@@ -15,35 +15,52 @@ using std::endl;
  * @param argv arguments passed (just like regular C )
  * @return int 
  */
-int sc_main(int argc, char *argv[]) 
+int sc_main(int argc, char *argv[])
 {
-	sc_signal<bool> clk("clk"); 
-	sc_signal<bool> reset("reset"); 
-	sc_signal<bool> enable("enable"); 
-	sc_signal<float, SC_MANY_WRITERS> stream("stream");	// AC_MANY_WRITERS allows stream to have numerous drivers, (dma and dma_test)
-	
+	sc_signal<bool> clk("clk");
+	sc_signal<bool> reset("reset");
+	sc_signal<bool> enable("enable");
+	sc_signal<float, SC_MANY_WRITERS> stream("stream"); // AC_MANY_WRITERS allows stream to have numerous drivers, (dma and dma_test)
+
 	// RAM representing external Memory
 	float ram[1000] = {0};
 
 	// Fill ram with 1-100
 	for (int i = 0; i < 100; i++)
-    {
+	{
 		ram[i] = i;
-    }
+	}
 
 	// DMA for MM2S S2MM instantiation
-	DMA_2D dma_temp("DMA_2D_MM2S", DmaDirection::MM2S, clk, reset, enable, ram, stream);
-	DMA_2D dma_temp("DMA_2D_S2MM", DmaDirection::S2MM, clk, reset, enable, ram, stream);
+	DMA_2D dma_MM2S("DMA_2D_MM2S", DmaDirection::MM2S, clk, reset, enable, ram, stream);
+	DMA_2D dma_S2MM("DMA_2D_S2MM", DmaDirection::S2MM, clk, reset, enable, ram, stream);
 
-    //1D transfer test
+	// 1D transfer test
+	unsigned int transfer_1D_blob_size = 10;
+	Descriptor_2D desc_s2mm_Transfer_1D_transfer = {2, 0, DmaState::TRANSFER, transfer_1D_blob_size, 1, 0, 0};
+	Descriptor_2D desc_s2mm_Transfer_1D_suspend_after = {0, 0, DmaState::SUSPENDED, 0, 1};
 
-    //2D transfer test
+	
+	Descriptor_2D desc_mm2s_Transfer_1D_wait_before = {1, 0, DmaState::WAIT, 1, 1, 0, 0};
+	Descriptor_2D desc_mm2s_Transfer_1D_transfer = {2, 0, DmaState::TRANSFER, transfer_1D_blob_size, 1, 0, 0};
+	Descriptor_2D desc_mm2s_Transfer_1D_suspend_after = {0, 0, DmaState::SUSPENDED, 0, 1};
 
-    //1D Strided Transfer Test
+	// Start Programming Sequence
+	dma_MM2S.loadProgram({desc_s2mm_Transfer_1D_transfer,
+						  desc_s2mm_Transfer_1D_suspend_after});
 
-    //2D Strided Transfer Test
+	dma_S2MM.loadProgram({desc_mm2s_Transfer_1D_wait_before,
+						  desc_mm2s_Transfer_1D_transfer,
+						  desc_mm2s_Transfer_1D_suspend_after});
+
+
+
+
+	//2D transfer test
+
+	//1D Strided Transfer Test
+
+	//2D Strided Transfer Test
 
 	return 0;
 }
-
-
--- src/components/Processor/tb_processor.cpp
+++ src/components/Processor/tb_processor.cpp
@@ -137,11 +137,7 @@ int sc_main(int argc, char *argv[])
   enable = 1;
   
   std::cout << "@" << sc_time_stamp() << " Transfer Start " << std::endl;
-  for(int i = 0; i<10; i++)
-  {
 
-  }
-  
   for (int i = 0; i < BIG_RAM_SIZE + 1; i++)
   {
     clk = 1;
