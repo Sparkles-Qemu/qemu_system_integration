dc16402fafe80237e35af588e63e442ecf9e7af2
asultan123 <asultan@aucegypt.edu>
2020-08-10 14:28:28 -0400

    some semblance of stability

---
+++ src/components/MemHierarchy/debug.el
@@ -0,0 +1,8 @@
+(dap-register-debug-template
+   "GDB::Run"
+   (list :type "gdb"
+         :request "launch"
+         :name "GDB::Run"
+         :valuesFormatting "prettyPrinters"
+         :target "tb"
+         :cwd "/home/sultan/Documents/research/qemu_system_integration/src/components/MemHierarchy"))
